442fe271d254bb3f57cfcbd10a4ce354abc79a7c
taroBill <bill890914@gmail.com>
2023-04-22 15:24:56 +0800

    修改groupbuying命名問題

---
+++ groupOrdering/Domain/GroupBuying.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace groupOrdering.Domain
+{
+    internal class GroupBuying
+    {
+        private Store _store;
+        private DateTime _endTime;
+
+        public GroupBuying()
+        {
+
+        }
+
+        public void ChooseExistStore(string storeID)
+        {
+
+        }
+
+        public void SetEndTime(DateTime endTime)
+        {
+
+        }
+
+        public void PublishGroupBuying()
+        {
+
+        }
+
+        public void SetGroupBuying(string userID)
+        {
+
+        }
+
+        public void CreateMemberOrder()
+        {
+
+        }
+
+        public void EndGroupBuying()
+        {
+
+        }
+
+        public void ListItemsOfStore()
+        {
+
+        }
+
+        public void AddItem(string userID, string itemID, int quantity)
+        {
+
+        }
+
+        public int GetTotalPrice()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
