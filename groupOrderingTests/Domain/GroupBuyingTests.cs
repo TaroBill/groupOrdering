1d6be2b7bfcf03dd0cbd58685502a88c75503888
taroBill <bill890914@gmail.com>
2023-04-22 16:12:14 +0800

    創建Unit Tests

---
+++ groupOrderingTests/Domain/GroupBuyingTests.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using groupOrdering.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace groupOrdering.Domain.Tests
+{
+    [TestClass()]
+    public class GroupBuyingTests
+    {
+        [TestMethod()]
+        public void GroupBuyingTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ChooseExistStoreTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SetEndTimeTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void PublishGroupBuyingTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SetGroupBuyingTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreateMemberOrderTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EndGroupBuyingTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ListItemsOfStoreTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddItemTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetTotalPriceTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ groupOrderingTests/Domain/MemberOrderTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using groupOrdering.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace groupOrdering.Domain.Tests
+{
+    [TestClass()]
+    public class MemberOrderTests
+    {
+        [TestMethod()]
+        public void MemberOrderTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CalculateDebtTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetTotalTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddItemTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
