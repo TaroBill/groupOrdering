623a40cc60ecdc0083da684ef94315211a296ab0
DESKTOP-D72U8OL\twm20 <t108590031@ntut.org.tw>
2023-04-24 16:28:51 +0800

    ADO class and ADO TEST

---
+++ groupOrderingTests/Technical/DAOTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using groupOrdering.Technical;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using groupOrdering.Domain;
+
+
+namespace groupOrdering.Technical.Tests
+{
+    [TestClass()]
+    public class DAOTests
+    {
+        [TestMethod()]
+        public void GetDataTest()
+        {
+            DAO dao = new DAO();
+            List<Store> store = dao.GetData<Store>("SELECT * FROM groupordering.store WHERE serverID='test';");
+            Assert.AreEqual(1, store[0].StoreID);
+            Assert.AreEqual("7-11", store[0].StoreName);
+            Assert.AreEqual("台北市", store[0].StoreAddress);
+            Assert.AreEqual("0909000000", store[0].StorePhoneNumber);
+        }
+    }
+}
